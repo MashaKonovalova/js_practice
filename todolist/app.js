2fa449d3bc4b19b876f4eb440472d632c9652d36
MashaKonovalova <75724191+MashaKonovalova@users.noreply.github.com>
2021-07-09 16:25:46 +0300

    new

--- todolist/app.js
+++ todolist/app.js
@@ -1,24 +1,25 @@
-let buttonEnter = document.getElementById('enter')
-let usetInput = document.getElementById('userInput')
-let ul = document.querySelector('ul')
+let buttonEnter = document.getElementById('enter');
+let usetInput = document.getElementById('userInput');
+let ul = document.querySelector('ul');
 
-function inputLenght() {
-    return userInput.value.lenght>0
+function inputLength() {
+    return userInput.value.length > 0 
 }
 function createTodo(){
     let li = document.createElement("li");
-    li.appendChild(document.createTextNode(usetInput.value));
+    li.appendChild(document.createTextNode(userInput.value));
     ul.appendChild(li);
     userInput.value = '';
-
-    let deleteButton = document.createElement('button');
+    /*let deleteButton = document.createElement('button');
     deleteButton.appendChild(document.createTextNode('x'));
     deleteButton.addEventListener('click', deleteTododItems);
+    */
 }
-
 function changeListAfterKeypress(event){
-    if(inputLenght() && event.which --- 13)
+    if(inputLength() && event.which===13);
     {
-        alert('key press!');
+        createTodo();
+        //alert('key press!');
     }
 }
+userInput.addEventListener('keypress',changeListAfterKeypress);
