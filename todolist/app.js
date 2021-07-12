5c2dfdf83991b8e4aacd06be9915fc8770a4519e
MashaKonovalova <75724191+MashaKonovalova@users.noreply.github.com>
2021-07-12 10:34:45 +0300

    new

--- todolist/app.js
+++ todolist/app.js
@@ -1,25 +1,42 @@
-let buttonEnter = document.getElementById('enter');
-let usetInput = document.getElementById('userInput');
-let ul = document.querySelector('ul');
+let buttonEnter = document.getElementById(`enter`);
+let userInput = document.getElementById(`userInput`);
+let ul = document.querySelector(`ul`)
 
-function inputLength() {
-    return userInput.value.length>0;
+function inputLenght(){
+return userInput.value.length > 0
 }
+
 function createTodo(){
-    let li = document.createElement("li");
-    li.appendChild(document.createTextNode(userInput.value));
-    ul.appendChild(li);
-    userInput.value = '';
-    /*let deleteButton = document.createElement('button');
-    deleteButton.appendChild(document.createTextNode('x'));
-    deleteButton.addEventListener('click', deleteTododItems);
-    */
+let li = document.createElement(`li`)
+li.appendChild(document.createTextNode(userInput.value))
+ul.appendChild(li)
+userInput.value = ``
+
+let deleteButton = document.createElement(`button`)
+deleteButton.appendChild(document.createTextNode(`X`))
+deleteButton.addEventListener(`click`,deleteTodoitems)
+li.appendChild(deleteButton)
+
+li.onclick = () =>{
+li.classList.toggle(`done`)
 }
-function changeListAfterKeypress(event){
-    if(inputLength() && event.which===13);
-    {
-        createTodo();
-        //alert('key press!');
-    }
+
+function deleteTodoitems(){
+li.classList.add(`delete`)
 }
-userInput.addEventListener('keypress',changeListAfterKeypress);
+}
+
+function changeListAfterKeyPress(event){
+if(inputLenght() && event.which === 13 ){
+createTodo()
+}
+
+}
+
+buttonEnter.onclick = () =>{
+if(inputLenght()){
+createTodo()
+}
+}
+
+userInput.addEventListener('keypress', changeListAfterKeyPress)
