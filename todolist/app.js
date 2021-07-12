a8e58ecf0f156032b60d7521a5e52c1779038a36
MashaKonovalova <75724191+MashaKonovalova@users.noreply.github.com>
2021-07-12 11:08:25 +0300

    new

--- todolist/app.js
+++ todolist/app.js
@@ -1,42 +1,46 @@
-let buttonEnter = document.getElementById(`enter`);
-let userInput = document.getElementById(`userInput`);
-let ul = document.querySelector(`ul`)
+$(function(){
 
-function inputLenght(){
-return userInput.value.length > 0
-}
+    let buttonEnter = $(`#enter`);
+    let userInput = $(`#userInput`);
+    let ul = document.querySelector(`ul`)
+    
+    function inputLenght(){
+         return !!userInput.val() > 0
+    }
+    
+    function createTodo(){
+    let li = document.createElement(`li`)
+    li.appendChild(document.createTextNode(userInput.val()))
+    ul.appendChild(li)
+    userInput.val(``)
+    
+    let deleteButton = document.createElement(`button`)
+    deleteButton.appendChild(document.createTextNode(`X`))
+    deleteButton.addEventListener(`click`,deleteTodoitems)
+    li.appendChild(deleteButton)
+    
+    li.onclick = () =>{
+        li.classList.toggle(`done`)
+    }
+    
+    function deleteTodoitems(){
+        li.classList.add(`delete`)
+        }
+    }
+    
+    function changeListAfterKeyPress(event){
+        if(inputLenght() && event.which === 13 ){
+        createTodo()
+        }
+    
+    }
+    
+    buttonEnter.click(function() {
+        if(inputLenght()){
+             createTodo()
+        }
+    });
+    
+    userInput.keypress(changeListAfterKeyPress)
+})
 
-function createTodo(){
-let li = document.createElement(`li`)
-li.appendChild(document.createTextNode(userInput.value))
-ul.appendChild(li)
-userInput.value = ``
-
-let deleteButton = document.createElement(`button`)
-deleteButton.appendChild(document.createTextNode(`X`))
-deleteButton.addEventListener(`click`,deleteTodoitems)
-li.appendChild(deleteButton)
-
-li.onclick = () =>{
-li.classList.toggle(`done`)
-}
-
-function deleteTodoitems(){
-li.classList.add(`delete`)
-}
-}
-
-function changeListAfterKeyPress(event){
-if(inputLenght() && event.which === 13 ){
-createTodo()
-}
-
-}
-
-buttonEnter.onclick = () =>{
-if(inputLenght()){
-createTodo()
-}
-}
-
-userInput.addEventListener('keypress', changeListAfterKeyPress)
